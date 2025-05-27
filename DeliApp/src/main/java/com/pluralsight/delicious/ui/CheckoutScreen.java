484e4b7016ffeba16454e1110cd39aba1089d971
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2025-05-27 13:24:20 -0400

    printing sandwich info at checkout

--- DeliApp/src/main/java/com/pluralsight/delicious/ui/CheckoutScreen.java
+++ DeliApp/src/main/java/com/pluralsight/delicious/ui/CheckoutScreen.java
@@ -1,10 +1,12 @@
 package com.pluralsight.delicious.ui;
 
+import com.pluralsight.delicious.models.Customer;
 import com.pluralsight.delicious.models.Order;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class CheckoutScreen implements ScreenState{
+public class CheckoutScreen implements ScreenState {
     @Override
     public void display() {
         System.out.println("Enter the name for your order?");
@@ -12,6 +14,24 @@ public class CheckoutScreen implements ScreenState{
 
     @Override
     public ScreenState handleInput(Scanner scanner, Order currentOrder) {
-        return null;
+        scanner.nextLine();
+        System.out.println("Here is your order");
+        System.out.println(currentOrder);
+        System.out.println("Enter 1) Confirm or 0) Cancel");
+        int input;
+        do {
+            input = scanner.nextInt();
+            switch (input) {
+                case 1 -> {
+                    System.out.println("TODO: add order to receipt folder");
+                    System.out.println(currentOrder);
+                }
+                case 2 -> {
+                    currentOrder = new Order(new ArrayList<>());
+
+                }
+            }
+        } while (input != 0 && input != 1);
+        return new OrderScreen();
     }
 }
