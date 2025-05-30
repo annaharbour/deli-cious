f1eb2c363131dcd059a1e1e8eeab9dda1bc0b233
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2025-05-29 23:37:07 -0400

    fuzzing app

--- DeliApp/src/main/java/com/pluralsight/delicious/ui/AddSandwichScreen.java
+++ DeliApp/src/main/java/com/pluralsight/delicious/ui/AddSandwichScreen.java
@@ -1,9 +1,11 @@
 package com.pluralsight.delicious.ui;
 
 import com.pluralsight.delicious.models.Order;
+import com.pluralsight.delicious.models.Sandwich;
 import com.pluralsight.delicious.ui.utils.ClearScreen;
 import com.pluralsight.delicious.ui.utils.PrintColored;
 
+import javax.swing.text.View;
 import java.util.Scanner;
 
 public class AddSandwichScreen implements ScreenState {
@@ -17,12 +19,16 @@ public class AddSandwichScreen implements ScreenState {
     @Override
     public ScreenState handleInput(Scanner scanner, Order currentOrder) {
         PrintColored.printColored("What kind of sandwich would you like to order?", "yellow");
-        String[] options = {"1) Create Custom Sandwich", "2) View Signature Sandwiches"};
+        String[] options = {"0) Back", "1) Create Custom Sandwich ", "2) View Signature Sandwiches"};
         for(String option: options){
             System.out.println("\t" + option);
         }
         int input = scanner.nextInt();
         return switch (input) {
+            case 0 -> {
+                ClearScreen.clearScreen();
+                yield new OrderScreen();
+            }
             case 1 -> {
                 ClearScreen.clearScreen();
                 yield new CustomizeSandwichScreen();
@@ -34,7 +40,7 @@ public class AddSandwichScreen implements ScreenState {
             default -> {
                 PrintColored.printColored("Invalid selection", "red");
                 ClearScreen.clearScreen();
-                yield this;
+                yield new AddSandwichScreen();
             }
 
         };
